3c9ba7ca586cd74ca2dd849d71200f10695b8401
BillyNaCl <1350463661@qq.com>
2026-04-01 20:19:55 +0800

    main函数，一些接口，以及一部分帮助文档

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using BillyNaCl.QQGroupToolkit.Interfaces;
+
+class MainLoop
+{
+    static void Main()
+    {
+        while (true)
+        {
+            string? input = Console.ReadLine();
+            if (input is "exit" or "quit" or "q")
+            {
+                break;
+            }
+        }
+        Console.WriteLine("程序已退出，按任意键关闭此窗口。");
+        Console.ReadKey();
+    }
+}
